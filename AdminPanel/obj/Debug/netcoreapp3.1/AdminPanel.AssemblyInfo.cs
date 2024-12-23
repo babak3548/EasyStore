9d4b15b737ad0ab2898e5cf696d4fc765a18fba7
Babak <babak3548@gmail.com>
2024-12-23 12:38:48 -0500

    AssemblyInfo confilict

--- AdminPanel/obj/Debug/netcoreapp3.1/AdminPanel.AssemblyInfo.cs
+++ AdminPanel/obj/Debug/netcoreapp3.1/AdminPanel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+132ec2acef312b46d5ed8864c51302f039993eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a4d41c778a94a0d10b80cd3f281a626ace0426")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
